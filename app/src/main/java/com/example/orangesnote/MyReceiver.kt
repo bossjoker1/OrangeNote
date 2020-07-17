0173ed02f0a614fec785ad5e57e2c6249e02ffcc
bossjoker1 <1397157763@qq.com>
2020-07-17 22:22:46 +0800

    编辑功能总算搞好了，开始都准备放弃这个了，另外增添了背景图，美化一下嘿嘿

---
+++ app/src/main/java/com/example/orangesnote/MyReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.orangesnote
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        Toast.makeText(context,"You have a task to do!!!", Toast.LENGTH_LONG).show()
+    }
+}
--- app/src/main/java/com/example/orangesnote/NoteActivity.kt
+++ app/src/main/java/com/example/orangesnote/NoteActivity.kt
@@ -1,17 +1,29 @@
 package com.example.orangesnote
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.orangesnote.data.AppDatabase
 import kotlinx.android.synthetic.main.activity_note.*
+import java.util.*
 import kotlin.concurrent.thread
+import kotlin.math.abs
 
 
-class NoteActivity : AppCompatActivity() {
+class NoteActivity : AppCompatActivity(){
+
+    private var am: AlarmManager? = null
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
@@ -26,15 +38,15 @@ class NoteActivity : AppCompatActivity() {
         val noteDao = AppDatabase.getDatabase(this).noteDao()
         submit.setOnClickListener {
             if (titleAdd.text.toString() != "") {
-                if (title!=""){
+                if (title != "") {
                     thread {
                         val tempNote = noteDao.loadById(id.toLong())
                         tempNote.title = titleAdd.text.toString()
                         tempNote.content = contentAdd.text.toString()
                         noteDao.updateNote(tempNote)
-                        Log.d("fuck0", tempNote.title+", "+tempNote.content)
+                        Log.d("fuck0", tempNote.title + ", " + tempNote.content)
                     }
-                }else {
+                } else {
                     thread {
                         noteDao.insertNote(
                             Note(
@@ -50,6 +62,50 @@ class NoteActivity : AppCompatActivity() {
             }
         }
 
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel("time", "Time", NotificationManager.IMPORTANCE_DEFAULT)
+        manager.createNotificationChannel(channel)
+
+        setBtn.setOnClickListener { view ->
+            val c = Calendar.getInstance()
+            val tz = TimeZone.getTimeZone("Asia/Shanghai")
+            c.setTimeZone(tz)
+            val hour = c?.get(Calendar.HOUR)
+            val minute = c?.get(Calendar.MINUTE);
+            val second = c?.get(Calendar.SECOND)
+            val mIntent = Intent(this, MyReceiver::class.java)
+            val mPendingIntent =
+                PendingIntent.getBroadcast(this, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            am = this
+                .getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            if (hour != null && minute != null) {
+                var interval = abs(
+                    3600 * (setHour.text.toString().toInt() - hour)
+                            + 60 * (setMin.text.toString().toInt() - minute)
+                ).toLong()
+                Log.d("fuck11", (setMin.text.toString().toInt() - minute).toString()+","+setMin.text.toString())
+                am!!.setExactAndAllowWhileIdle(
+                    AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + interval*1000,
+                    mPendingIntent
+                )
+                Log.d("fuck8", hour.toString() + "," + minute.toString() + "," + interval.toString())
+            }
+            /**本来想做个通知的,但是通知好像不如广播来的方便
+             * setBtn.setOnClickListener {
+            val notification = NotificationCompat.Builder(this, "time")
+            .setContentTitle("Time is up!")
+            .setContentText("Task: ${titleAdd.text.toString()} ")
+            .setSmallIcon(R.drawable.small_icon)
+            .setLargeIcon(BitmapFactory.decodeResource(resources,
+            R.drawable.large_icon))
+            .build()
+            runBlocking {
+            launch {
+            manager.notify(1, notification)
+            }
+            }
+            }*/
+        }
     }
    //监听并防止用户按back键直接返回到桌面，之前那个方法不行
     override fun onBackPressed() {
@@ -60,3 +116,4 @@ class NoteActivity : AppCompatActivity() {
          super.onBackPressed()
     }
 }
+
